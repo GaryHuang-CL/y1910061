2e9363d0061d4c3b252b66bbcdab7fba4f63129f
nerd.liny <nerd.liny@33d2cb69-a451-0410-ad08-8b135caa1f46>
2009-01-23 02:06:27 +0000

    test

--- bot/db.php
+++ bot/db.php
@@ -1,4 +1,4 @@
 <?php
-	if(!mysql_connect("localhost", "y1910061_travian", "travian")) die(mysql_error());
+	if(!mysql_connect("revconnect.com", "y1910061_travian", "travian")) die(mysql_error());
 	if(!mysql_select_db("y1910061_traviandb")) die(mysql_error());
 ?>
