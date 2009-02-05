d1df3d8bd4274b2f5bfe93ec759aa34b54b5dddc
nerd.liny <nerd.liny@33d2cb69-a451-0410-ad08-8b135caa1f46>
2009-02-05 01:55:40 +0000

    aaa

--- bot/catapult.sh
+++ bot/catapult.sh
@@ -1,5 +1,5 @@
-/usr/local/bin/php catapult-child.php "kata=15&id=39&a=63138&c=3&kid=369571&t1=0&t2=0&t3=0&t4=0&t5=0&t6=0&t7=0&t8=1&t9=0&t10=0&t11=0&s1.x=&s1.y=&s1=ok" 0 &
-/usr/local/bin/php catapult-child.php "kata=11&id=39&a=4369&c=3&kid=369571&t1=0&t2=0&t3=0&t4=0&t5=0&t6=0&t7=0&t8=1&t9=0&t10=0&t11=0&s1.x=&s1.y=&s1=ok" 250000 &
-/usr/local/bin/php catapult-child.php "kata=25&id=39&a=53279&c=3&kid=369571&t1=0&t2=0&t3=0&t4=0&t5=0&t6=0&t7=0&t8=1&t9=0&t10=0&t11=0&s1.x=&s1.y=&s1=ok" 500000 &
-/usr/local/bin/php catapult-child.php "kata=10&id=39&a=41533&c=3&kid=369571&t1=0&t2=0&t3=0&t4=0&t5=0&t6=0&t7=0&t8=1&t9=0&t10=0&t11=0&s1.x=&s1.y=&s1=ok" 750000 &
-/usr/local/bin/php catapult-child.php "kata=10&id=39&a=40021&c=3&kid=369571&t1=0&t2=0&t3=0&t4=0&t5=0&t6=0&t7=0&t8=1&t9=0&t10=0&t11=0&s1.x=&s1.y=&s1=ok" 1000000 &
+php catapult-child.php "kata=15&kata2=&id=39&a=17433&c=3&kid=296866&t1=1633&t2=0&t3=0&t4=0&t5=0&t6=100&t7=100&t8=10&t9=0&t10=0&t11=0&s1.x=&s1.y=&s1=ok" 0 &
+php catapult-child.php "kata=8&kata2=9&id=39&a=1848&c=3&kid=296866&t1=0&t2=0&t3=25&t4=0&t5=0&t6=0&t7=0&t8=20&t9=0&t10=0&t11=0&s1.x=&s1.y=&s1=ok" 200000 &
+php catapult-child.php "kata=4&kata2=4&id=39&a=24204&c=3&kid=296866&t1=0&t2=0&t3=25&t4=0&t5=0&t6=0&t7=0&t8=20&t9=0&t10=0&t11=0&s1.x=&s1.y=&s1=ok" 400000 &
+php catapult-child.php "kata=4&kata2=4&id=39&a=47086&c=3&kid=296866&t1=0&t2=0&t3=25&t4=0&t5=0&t6=0&t7=0&t8=20&t9=0&t10=0&t11=0&s1.x=&s1.y=&s1=ok" 600000 &
+php catapult-child.php "kata=4&kata2=4&id=39&a=59384&c=3&kid=296866&t1=0&t2=0&t3=25&t4=0&t5=0&t6=0&t7=0&t8=20&t9=0&t10=0&t11=0&s1.x=&s1.y=&s1=ok" 800000 &
